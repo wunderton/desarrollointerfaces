077d06885764e0feeb5946d4fd8cbe6514bc4fd1
Antonio Carrasco <wunderton@gmail.com>
2019-10-21 08:28:58 +0200

    ejercicios Desarrollo interfaces

---
+++ Ej10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray {
+    class Program {
+        static void Main(string[] args) {
+            int total = 10;
+            int[] arr = new int[total];
+
+            String aux;
+
+            for (int i = 0; i < total; i++) {
+                Console.Write("\nIntroduce numeros: ");
+                aux = Console.ReadLine();
+                arr[i] = int.Parse(aux);
+            }
+
+            for (int i = 0; i < total; i++) {
+                Console.Write("[" + arr[i] + "]");
+            }
+
+            int menor = arr[total - 1];
+
+            for (int i = 0; i < total; i++) {
+                if (arr[i] < menor) {
+                    menor = arr[i];
+                }
+            }
+            Console.WriteLine("\n\nEl numero menor en el array fue: " + menor);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Ej9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3 {
+    class Program {
+        static void Main(string[] args) {
+
+            try {
+                Console.WriteLine("Escriba un numero: ");
+                String a = Console.ReadLine();
+                float num1 = int.Parse(a);
+
+                Console.WriteLine("Escriba otro numero: ");
+                String b = Console.ReadLine();
+                float num2 = int.Parse(b);
+
+                float resultado = num1 / num2;
+                Console.Write("Resultado: ");
+                Console.WriteLine(resultado);
+            }
+            catch (Exception e) {
+                Console.WriteLine("El caracter introducido no era un numero" + e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
